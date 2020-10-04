72d93447dd20620a94613eadd3452d3db81a0830
Jannis Mattheis <contact@jmattheis.de>
2020-10-04 19:23:49 +0200

    Add logger

---
+++ logger/logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+// Init initializes the logger
+func Init(lvl zerolog.Level) {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).Level(lvl)
+	log.Debug().Msg("Logger initialized")
+}
