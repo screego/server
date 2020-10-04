0602e758f4fb412d9650d3b6d3fe707483e22272
Jannis Mattheis <contact@jmattheis.de>
2020-10-04 19:27:00 +0200

    Add serve command

---
+++ cmd/command.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/urfave/cli"
+)
+
+func Run(version, commitHash string) {
+	app := cli.App{
+		Name:    "screego",
+		Version: fmt.Sprintf("%s; screego/server@%s", version, commitHash),
+		Commands: []cli.Command{
+			serveCmd,
+			hashCmd,
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal().Err(err).Msg("app error")
+	}
+}
---
+++ cmd/serve.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	mrand "math/rand"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/screego/server/auth"
+	"github.com/screego/server/config"
+	"github.com/screego/server/logger"
+	"github.com/screego/server/router"
+	"github.com/screego/server/server"
+	"github.com/screego/server/turn"
+	"github.com/screego/server/ws"
+	"github.com/urfave/cli"
+)
+
+var serveCmd = cli.Command{
+	Name: "serve",
+	Action: func(ctx *cli.Context) {
+
+		mrand.Seed(time.Now().Unix())
+		conf, errs := config.Get()
+		logger.Init(conf.LogLevel.AsZeroLogLevel())
+
+		exit := false
+		for _, err := range errs {
+			log.WithLevel(err.Level).Msg(err.Msg)
+			exit = exit || err.Level == zerolog.FatalLevel || err.Level == zerolog.PanicLevel
+		}
+		if exit {
+			os.Exit(1)
+		}
+		users, err := auth.ReadPasswordsFile(conf.UsersFile, conf.Secret)
+		if err != nil {
+			log.Fatal().Str("file", conf.UsersFile).Err(err).Msg("While loading users file")
+		}
+
+		auth, err := turn.Start(conf)
+		if err != nil {
+			log.Fatal().Err(err).Msg("could not start turn server")
+		}
+
+		rooms := ws.NewRooms(auth, users, conf)
+
+		go rooms.Start()
+
+		r := router.Router(conf, rooms, users)
+		if err := server.Start(r, conf.ServerAddress, conf.TLSCertFile, conf.TLSKeyFile); err != nil {
+			log.Fatal().Err(err).Msg("http server")
+		}
+	},
+}
