3376d82abfe0ef4195565c74371583423cd405e1
Jannis Mattheis <contact@jmattheis.de>
2020-10-04 19:26:45 +0200

    Add router

---
+++ router/router.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/screego/server/auth"
+	"github.com/screego/server/config"
+	"github.com/screego/server/ui"
+	"github.com/screego/server/ws"
+)
+
+type UIConfig struct {
+	AuthMode string `json:"authMode"`
+	User     string `json:"user"`
+	LoggedIn bool   `json:"loggedIn"`
+}
+
+func Router(conf config.Config, rooms *ws.Rooms, users *auth.Users) *mux.Router {
+	router := mux.NewRouter()
+	router.Use(handlers.CORS(handlers.AllowedMethods([]string{"GET", "POST"}), handlers.AllowedOriginValidator(conf.CheckOrigin)))
+	router.HandleFunc("/stream", rooms.Upgrade)
+	router.Methods("POST").Path("/login").HandlerFunc(users.Authenticate)
+	router.Methods("POST").Path("/logout").HandlerFunc(users.Logout)
+	router.Methods("GET").Path("/config").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, loggedIn := users.CurrentUser(r)
+		_ = json.NewEncoder(w).Encode(&UIConfig{
+			AuthMode: conf.AuthMode,
+			LoggedIn: loggedIn,
+			User:     user,
+		})
+	})
+
+	ui.Register(router)
+
+	return router
+}
