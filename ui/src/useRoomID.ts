4aed9ad65791e62d41b959191503046500834889
kim <35963228+king54346@users.noreply.github.com>
2025-05-21 10:31:42 +0800

    Update useRoomID.ts

--- ui/src/useRoomID.ts
+++ ui/src/useRoomID.ts
@@ -1,35 +1,30 @@
-import React from 'react';
-
-export const getRoomFromURL = (): string | undefined => getFromURL('room');
-
-export const getFromURL = (
-    key: string,
-    search: string = window.location.search
-): string | undefined =>
+export const getFromURL = (key: string, search: string = window.location.search): string | undefined =>
     search
         .slice(1)
         .split('&')
         .find((param) => param.startsWith(`${key}=`))
         ?.split('=')[1];
 
+export const getRoomFromURL = (): string | undefined => getFromURL('room');
+
 export const useRoomID = (): [string | undefined, (v?: string) => void] => {
-    const [state, setState] = React.useState<string | undefined>(() => getRoomFromURL());
+    const [RoomId, setRoomId] = React.useState<string | undefined>(() => getRoomFromURL());
     React.useEffect(() => {
-        const onChange = (): void => setState(getRoomFromURL());
+        const onChange = (): void => setRoomId(getRoomFromURL());
         window.addEventListener('popstate', onChange);
         return () => window.removeEventListener('popstate', onChange);
-    }, [setState]);
+    }, [RoomId]);
     return [
-        state,
+        RoomId,
         React.useCallback(
             (id) =>
-                setState((oldId?: string) => {
+                setRoomId((oldId?: string) => {
                     if (oldId !== id) {
                         window.history.pushState({roomId: id}, '', id ? `?room=${id}` : '?');
                     }
                     return id;
                 }),
-            [setState]
+            [RoomId]
         ),
     ];
 };
